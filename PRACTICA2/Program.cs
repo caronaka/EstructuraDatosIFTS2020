0ce66c8963313c35c3fa8c4df422ebafd9dcb992
LAPTOP-VJ6UVJ04\Caro <Caro>
2020-09-06 15:48:26 -0300

    Clase 3 - 4septiembre

---
+++ PRACTICA2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRACTICA2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Clase 3
+
+            int i;
+            for(i=0; i<5; i++)
+            {
+                Console.WriteLine("Hola Mundo");
+                Console.ReadKey(); //para que no se cierre la consola
+            }
+
+
+        }
+    }
+}
