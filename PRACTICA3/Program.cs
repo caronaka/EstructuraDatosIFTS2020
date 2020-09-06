7f9b7cc9fe18207a85a462eadc5ce736efb38180
LAPTOP-VJ6UVJ04\Caro <Caro>
2020-09-06 16:03:34 -0300

    cambio de nombres

---
+++ PRACTICA3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRACTICA2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Clase 3
+
+            //BUBLE FOR
+
+            int i; 
+            for(i=0; i<5; i++)
+            {
+                Console.WriteLine("Hola Mundo");
+                Console.ReadKey(); //para que no se cierre la consola
+            }
+
+
+            //
+
+        }
+    }
+}
